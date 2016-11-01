e8a9dd547e2a81d6449a2c1db074ca00d4b2fbe7
TZH <zhihuangtang@gmail.com>
2016-11-01 22:31:38 +0800

    修复混乱版本.

--- app/src/main/java/com/qg/kinectpatient/result/Result.java
+++ app/src/main/java/com/qg/kinectpatient/result/Result.java
@@ -1,5 +1,7 @@
 package com.qg.kinectpatient.result;
 
+import android.text.TextUtils;
+
 /**
  * Created by ZH_L on 2016/10/21.
  */
@@ -7,7 +9,20 @@ public class Result {
     public int status = 1;
     public String errMsg = "";
 
-    public boolean isOk(){
-        return status != 0;
+    public boolean isOk() {
+        return status == 1;
+    }
+
+    public String getErrMsg() {
+        String errMsg = this.errMsg;
+        if (!TextUtils.isEmpty(errMsg)) {
+            return errMsg;
+        } else {
+            return otherErrMsg();
+        }
+    }
+
+    protected String otherErrMsg() {
+        return "未知错误";
     }
 }
